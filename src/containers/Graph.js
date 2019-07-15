00a2d4b6e3e5a53cf156b792fb9db364f8a76cd8
モガ太郎 <mogashiro@gmail.com>
2019-07-16 00:37:56 +0900

    Fix

--- src/containers/Graph.js
+++ src/containers/Graph.js
@@ -10,23 +10,22 @@ import ClearBtn from '../components/ClearBtn';
 import Result from '../components/Result';
 
 class Graph extends Component {
+  // ページロード時にactions.fetchPrefectures()を実行する
+  componentDidMount() {
+    const { actions } = this.props
+    actions.fetchPrefectures()
+  }
   render() {
-    console.log(this.props);
     const { prefectures, actions } = this.props;
     const prefArray = prefectures.prefectures
-    const maxCount = prefArray.length
-    const prefNames = [];
-    //アイテムを10件生成
-    for (let i = 0; i < maxCount; i++) {
-      prefNames.push(
-        <p>{prefArray[i].prefName}</p>
-      )
-    }
+    const prefNames = prefArray.map((p, i) => <p key={i}>{p.prefName}</p>)
+    const hoge = '<p>ほげ</p>'
     return (
       <div>
         <div>
           <p>RESAS API</p>
           <button onClick={() => actions.fetchPrefectures()}>都道府県を取得</button>
+          <div dangerouslySetInnerHTML={{ __html: hoge }}></div>
           {prefNames}
         </div>
       </div>
